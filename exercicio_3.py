e3d005b4c26c15e768bae6b9c50aaa2827d054d4
LourraineR <163673448+LourraineR@users.noreply.github.com>
2024-04-04 20:18:23 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -62,7 +62,7 @@ genoma_referencia = (input("Digite o genoma de referência:"))
 
 if numero_cromossomo == "chr17":
     print("Resposta: ")
-if genoma_referencia == "hg19":
+elif genoma_referencia == "hg19":
     41196312<= posicao_variante <=41277500
     print("Sim")
 elif genoma_referencia == "hg38":
@@ -71,4 +71,3 @@ elif genoma_referencia == "hg38":
     
 else:
     print("Resposta: Não")
-    
